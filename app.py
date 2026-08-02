5828e4e91ad63fe2341f452025b16f629d6934bc
Achan-App <aibenkyo777@gmail.com>
2026-08-02 22:08:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -58,8 +58,7 @@ if uploaded_file is not None:
                 
                 try:
                     # テキスト生成モデルを使用
-                    # 変更後
-                    model = genai.GenerativeModel('gemini-2.0-flash')
+                    model = genai.GenerativeModel('gemini-2.5-flash')
                     response = model.generate_content(
                         prompt,
                         generation_config={"response_mime_type": "application/json"}
